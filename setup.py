5ec5b0be57c749afca5e6bd79fb818e0bef2e08c
Rituparna Addy <93533786+Ritu000addy@users.noreply.github.com>
2023-04-18 13:09:00 +0530

    update

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+
+name= 'mlproject',
+version= '0.0.1',
+author= 'Rituparna',
+author_email= 'addy.rituparna@gmail.com',
+packages= find_packages(),
+install_requires= ['pandas', 'seaborn', 'numpy'],
+
+)
